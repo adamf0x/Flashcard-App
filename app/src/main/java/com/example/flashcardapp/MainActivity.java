13bb3260358f9f55f6e0b1e10751003ee2b78b01
Adam Fox <adamfox96@gmail.com>
2020-03-22 15:12:23 -0700

    creation of category functionality added

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -2,7 +2,17 @@ package com.example.flashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> categories = new ArrayList<>();
+
+        try{
+            FileInputStream fis = openFileInput("categories.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String line = br.readLine();
+            while(line != null) {
+                categories.add(line);
+                line = br.readLine();
+            }
+            br.close();
+            isr.close();
+            fis.close();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, categories);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.category);
+        sItems.setAdapter(adapter);
+    }
+
+    public void viewCategory(View view) {
+
+    }
+
+    public void editCategory(View view) {
+    }
+
+    public void createCategory(View view) {
+        Intent intent = new Intent(this,createCategory.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/flashcardapp/createCategory.java
+++ app/src/main/java/com/example/flashcardapp/createCategory.java
@@ -2,7 +2,13 @@ package com.example.flashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.FileOutputStream;
 
 public class createCategory extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class createCategory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_category);
+    }
+
+
+    public void createCategory(View view) {
+        TextView catName = findViewById(R.id.editText2);
+        CharSequence category = catName.getText();
+        category = category.toString();
+
+        String filename = "categories.txt";
+        String fileContents= category + "\n";
+        FileOutputStream outputStream; //allow a file to be opened for writing
+        try {
+            outputStream= openFileOutput(filename, Context.MODE_APPEND);
+            outputStream.write(fileContents.getBytes());
+            outputStream.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
