9cbe1ccdd41f6aead4219878dc2fe1353b88b68e
adamf0x <adamfox96@gmail.com>
2020-03-22 23:29:27 -0700

    changed operation of set creation

--- app/src/main/java/com/example/flashcardapp/CreateSet.java
+++ app/src/main/java/com/example/flashcardapp/CreateSet.java
@@ -23,38 +23,29 @@ public class CreateSet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_set);
-        ArrayList<String> categories = new ArrayList<>();
-        try{
-            FileInputStream fis = openFileInput("categories.txt");
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader br = new BufferedReader(isr);
-            String line = br.readLine();
-            while(line != null) {
-                categories.add(line);
-                line = br.readLine();
-            }
-            br.close();
-            isr.close();
-            fis.close();
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_spinner_item, categories);
+        Bundle extras = getIntent().getExtras();
+        String value = "";
+        if (extras != null) {
+            value = extras.getString("category");
+        }
+        TextView cat = findViewById(R.id.textView23);
+        cat.setText(value);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner sItems = (Spinner) findViewById(R.id.category);
-        sItems.setAdapter(adapter);
     }
 
     public void done(View view) {
         TextView set = findViewById(R.id.editText3);
         CharSequence setDesc = set.getText();
 
+        Bundle extras = getIntent().getExtras();
+        String value = "";
+        if (extras != null) {
+            value = extras.getString("category");
+        }
+
         String filename = "sets.txt";
-        String fileContents= setDesc + "\n";
+        String fileContents= value + "," + setDesc + "\n";
         FileOutputStream outputStream; //allow a file to be opened for writing
         try {
             outputStream= openFileOutput(filename, Context.MODE_APPEND);
--- app/src/main/java/com/example/flashcardapp/SelectSet.java
+++ app/src/main/java/com/example/flashcardapp/SelectSet.java
@@ -2,8 +2,18 @@ package com.example.flashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class SelectSet extends AppCompatActivity {
 
@@ -11,9 +21,26 @@ public class SelectSet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_set);
+
+
+        Bundle extras = getIntent().getExtras();
+        String value = "";
+        if (extras != null) {
+            value = extras.getString("category");
+        }
+        System.out.println(value);
+        TextView category = findViewById(R.id.textView22);
+        category.setText(value);
     }
 
     public void createSet(View view) {
+        TextView category = findViewById(R.id.textView22);
+        Intent intent = new Intent(this, CreateSet.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("category", category.getText().toString());
+
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 
     public void editSet(View view) {
@@ -23,5 +50,10 @@ public class SelectSet extends AppCompatActivity {
     }
 
     public void deleteSet(View view) {
+    }
+
+    public void home(View view) {
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
     }
 }
